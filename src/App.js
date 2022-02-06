033786ddc4d1807ff5aab356b8a7566a3e58400c
Iriko0606 <eriko.i0606@gmail.com>
2022-02-06 13:41:08 +0900

    feature:画面のbase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Top from './pages/Top'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/Watch" element={<Watch />} />
+      </Routes>
+      </Router>
   );
 }
 
---
+++ src/pages/Top.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Top = () => {
+  return(
+      <Layout>
+        TopPages
+      </Layout>
+  )
+}
+
+export default Top
